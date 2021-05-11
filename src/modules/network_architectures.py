eaed86ca660728c29bc85c15067ef48567be2766
Jwalin Bhatt <jbhatt@rhrk.uni-kl.de>
2021-05-11 12:56:43 +0000

    Replace network_architectures.py

--- src/modules/network_architectures.py
+++ src/modules/network_architectures.py
@@ -7,20 +7,20 @@ class AlexNet(nn.Module):
 
   def __init__(self, num_classes:int=2, inp_channels:int=3) -> None:
     super(AlexNet, self).__init__()
-    self.features = nn.Sequential(
-      nn.Conv1d(inp_channels, 64, kernel_size=11, stride=4, padding=2),
+    self.features = nn.Sequential(    # inp_len = 50             np.floor((Lin + 2*padding - kernel_size)/stride + 1)
+      nn.Conv1d(inp_channels, 64, kernel_size=11, stride=4, padding=2),   # 50 -> 11 
       nn.ReLU(inplace=True),
-      nn.MaxPool1d(kernel_size=3, stride=2),
-      nn.Conv1d(64, 192, kernel_size=5, padding=2),
+      nn.MaxPool1d(kernel_size=3, stride=2),    # 11 -> 5
+      nn.Conv1d(64, 192, kernel_size=5, padding=2), # same
       nn.ReLU(inplace=True),
-      nn.MaxPool1d(kernel_size=3, stride=2),
-      nn.Conv1d(192, 384, kernel_size=3, padding=1),
+      nn.MaxPool1d(kernel_size=3, stride=2),  # 5 -> 2
+      nn.Conv1d(192, 384, kernel_size=3, padding=1),  # same
       nn.ReLU(inplace=True),
-      nn.Conv1d(384, 256, kernel_size=3, padding=1),
+      nn.Conv1d(384, 256, kernel_size=3, padding=1),  # same
       nn.ReLU(inplace=True),
-      nn.Conv1d(256, 256, kernel_size=3, padding=1),
+      nn.Conv1d(256, 256, kernel_size=3, padding=1),  # same
       nn.ReLU(inplace=True),
-      nn.MaxPool1d(kernel_size=2, stride=2),
+      nn.MaxPool1d(kernel_size=2, stride=2),    # 2 -> 1
     )
     self.avgpool = nn.AdaptiveAvgPool1d(6)
     self.classifier = nn.Sequential(
