c86c70ff4a5c19fb8ae4a8cd83fdc8541cb187a4
Jwalin Bhatt <jbhatt@rhrk.uni-kl.de>
2021-05-19 11:16:31 +0000

    Replace attribution_f.py

--- src/modules/attribution_f.py
+++ src/modules/attribution_f.py
@@ -5,8 +5,7 @@ import matplotlib.pyplot as plt
 import torch
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def salMap(model, sample):
-
+def compute_grad(model, sample):
   sample = torch.from_numpy(sample).to(device).float()
   sample = sample.unsqueeze(0)
   _ = sample.requires_grad_()
@@ -18,46 +17,44 @@ def salMap(model, sample):
   # get the logits
   pred = model(sample)
   pred_class = pred.argmax(dim=1)[0]
+  #print("prediction:",pred_class)
   
   # grad wrt to predicted class
   pred[:, pred_class].backward()
+  return sample
 
-  # Retireve the saliency map and also pick the maximum value from channels on each time stamp.
+def applyMethod(method, model, samples):
+  maps = []
+  for sample in samples:
+    map1 = globals()[method](model, sample)
+    maps.append(map1)
+  return maps  
+
+def salMap(model, sample):
+  sample = compute_grad(model, sample)
+
   # In this case, we look at dim=1. Recall the shape (batch_size, channel, time_stamp)
-  saliency, _ = torch.max(sample.grad.data.abs(), dim=1)
-  saliency = saliency.view(-1).cpu()
-  impPts, impIndxs = saliency.sort(descending=True)
-  impPts, impIndxs = impPts[:5], impIndxs[:5]
+  saliency = sample.grad.data.abs().squeeze().cpu()
+  # adjust shape to [sample_len, n_channels]
+  saliency = saliency.transpose(0,1) if len(saliency.shape) > 1 else saliency.reshape(-1,1)
+  return saliency
 
-  # Visualize the image and the saliency map
-  fig, ax = plt.subplots(1, 2, figsize=(12,4))
-  ax[0].plot(sample.squeeze(0).transpose(0,1).cpu().detach().numpy())
-  ax[0].set_title("sample")
-  #ax[0].axis('off')
-  ax[1].plot(saliency)
-  ax[1].scatter(impIndxs,impPts)
-  ax[1].set_title("saliency map")
-  #ax[1].axis('off')
-  plt.tight_layout()
-  plt.show()
+def visualizeMaps(inputs, maps):
+  for sample, map1 in zip(inputs,maps):
+    # Visualize the sample and the map
+    fig, ax = plt.subplots(2, 1, figsize=(5,10))
+    ax[0].plot(sample.transpose(1,0))
+    ax[0].set_title("sample")
+    #ax[0].axis('off')
+    ax[1].plot(map1)
+    ax[1].set_title("map")
+    #ax[1].axis('off')
+    plt.tight_layout()
+    plt.show()
+    print()
 
 def gradCAM(model, sample):
-
-  sample = torch.from_numpy(sample).to(device).float()
-  sample = sample.unsqueeze(0)
-  _ = sample.requires_grad_()
-
-  model.eval()
-  # Retrieve output from the image
-  output = model(sample)
-
-  # get the logits
-  pred = model(sample)
-  pred_class = pred.argmax(dim=1)[0]
-  print("prediction:",pred_class)
-  
-  # grad wrt to predicted class
-  pred[:, pred_class].backward()
+  sample = compute_grad(model, sample)
 
   # pull the gradients out of the model
   gradients = model.get_activations_gradient().cpu().detach()
@@ -81,27 +78,28 @@ def gradCAM(model, sample):
 
   # normalize the heatmap
   heatmap /= torch.max(heatmap)
+  return heatmap
 
-  # Visualize the image and the saliency map
-  fig, ax = plt.subplots(3, 1, figsize=(5,10))
-  ax[0].plot(sample.squeeze(0).transpose(0,1).cpu().detach().numpy())
-  ax[0].set_title("sample")
-  #ax[0].axis('off')
+  # # Visualize the image and the saliency map
+  # fig, ax = plt.subplots(3, 1, figsize=(5,10))
+  # ax[0].plot(sample.squeeze(0).transpose(0,1).cpu().detach().numpy())
+  # ax[0].set_title("sample")
+  # #ax[0].axis('off')
 
-  # draw the heatmap
-  ax[1].plot(heatmap)
-  ax[1].set_title("saliency map")
-  #ax[1].axis('off')
+  # # draw the heatmap
+  # ax[1].plot(heatmap)
+  # ax[1].set_title("grad cam")
+  # #ax[1].axis('off')
 
-  # resize to plot superimposed series
-  n, ch, len1 = sample.shape
-  len2 = len(heatmap)
-  x = np.linspace(0,len2-1,len1)
-  xp = list(range(len2))
-  heatmap = np.interp(x,xp,heatmap)
-  ax[2].plot(heatmap, alpha=0.5)
-  ax[2].plot(sample.squeeze(0).transpose(0,1).cpu().detach().numpy())
-  ax[2].set_title("superimposed")
-  #ax[2].axis('off')
-  plt.tight_layout()
-  plt.show()  
+  # # resize to plot superimposed series
+  # n, ch, len1 = sample.shape
+  # len2 = len(heatmap)
+  # x = np.linspace(0,len2-1,len1)
+  # xp = list(range(len2))
+  # heatmap = np.interp(x,xp,heatmap)
+  # ax[2].plot(heatmap, alpha=0.5)
+  # ax[2].plot(sample.squeeze(0).transpose(0,1).cpu().detach().numpy())
+  # ax[2].set_title("superimposed")
+  # #ax[2].axis('off')
+  # plt.tight_layout()
+  # plt.show()  
