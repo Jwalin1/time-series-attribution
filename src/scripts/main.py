fddaac3926807f5bf9536061048e723b0a5782cb
Jwalin Bhatt <jbhatt@rhrk.uni-kl.de>
2021-05-19 11:17:42 +0000

    Replace main.py

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -6,7 +6,7 @@ from modules import data_f, network_f, network_architectures, attribution_f
 
 # reload module
 import importlib
-importlib.reload(network_architectures)
+importlib.reload(attribution_f)
 
 
 
@@ -17,7 +17,8 @@ def main(args):
   dataloaders = data_f.createLoaders(train_inputs, train_labels, test_inputs, test_labels, 32)
   trainValLoaders = {"train":dataloaders["train"], "val":dataloaders["val"]}
   n_samples, n_channels, sample_lens = train_inputs.shape
-  n_classes = len(np.unique(train_labels))
+  classes = np.unique(train_labels)
+  n_classes = len(classes)
 
   model,criterion,optimizer,scheduler = network_f.setupModel(network_architectures.AlexNet(n_classes, n_channels))
   if args.load is not None:
@@ -30,7 +31,13 @@ def main(args):
       network_f.save_state_dict(model, args.dataset)
   #network_f.evaluate(model, dataloaders["test"])
 
-  attribution_f.gradCAM(model,train_inputs[0])
+  # x = train_inputs[np.where(train_labels==0)]
+  # map1 = attribution_f.salMap(model,x[2])
+  # attribution_f.visualizeMaps([x[2]], [map1])
+  samplesPerClass = 2
+  selectedInputs = data_f.selectInputs(train_inputs, train_labels, samplesPerClass)
+  maps = attribution_f.applyMethod("salMap",model,selectedInputs)
+  attribution_f.visualizeMaps(selectedInputs, maps)
 
 
 
