3c944029ca90d7c7ddda03365d092ae0ad939a7a
Jwalin Bhatt <jbhatt@rhrk.uni-kl.de>
2021-05-23 14:52:39 +0000

    Replace attribution_f.py

--- src/modules/attribution_f.py
+++ src/modules/attribution_f.py
@@ -4,30 +4,47 @@ import matplotlib.pyplot as plt
 # for neural network
 import torch
 
+# to track progress
+from tqdm.notebook import tqdm
+
 # for attribution
+# https://captum.ai/api/attribution.html
 from captum.attr import Saliency, IntegratedGradients, InputXGradient, GuidedBackprop, GuidedGradCam, Lime
 from captum.attr import LayerGradCam, LayerAttribution
+
+# https://github.com/yiskw713/ScoreCAM
+from modules.cam import GradCAMpp, SmoothGradCAMpp, ScoreCAM
+# https://github.com/yiskw713/RISE
+from modules.rise import RISE
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def applyMethod(method, model, samples):
   model.eval()
   maps = []
-  for sample in samples:
+  for sample in tqdm(samples):
     
     sample = torch.from_numpy(sample).to(device).unsqueeze(0)
     sample = sample.float().requires_grad_(True)
     target = model(sample).argmax(dim=1)[0]
-    if method in ["LayerGradCam", "GuidedGradCam"]:
+    if method in ["LayerGradCam", "GuidedGradCam"] + ["GradCAMpp", "SmoothGradCAMpp", "ScoreCAM"]:
       interpreter = globals()[method](model, model.features[-3])
+    elif method == "RISE":
+      interpreter = globals()[method](model, input_size=(sample.shape[2]))
     else:
       interpreter = globals()[method](model)
       
-    attribution = interpreter.attribute(sample, target=target)
+    if method in ["GradCAMpp", "SmoothGradCAMpp", "ScoreCAM"]:
+      attribution, idx = interpreter(sample)
+    elif method == "RISE":
+      attribution = interpreter(sample)[target].view(1,1,-1)
+    else:  
+      attribution = interpreter.attribute(sample, target=target)
+
     if method in ["LayerGradCam", "GuidedGradCam"]:
       attribution = LayerAttribution.interpolate(attribution, sample.shape[2])
-    attribution = attribution.squeeze(0).detach().numpy()
 
+    attribution = attribution.squeeze(0).detach().numpy()
     maps.append(attribution)
   return maps
 
