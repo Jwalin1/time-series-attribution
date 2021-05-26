70a99c0541fa7532965bac2305fa3cc3084ff2b6
Jwalin Bhatt <jbhatt@rhrk.uni-kl.de>
2021-05-26 01:16:38 +0000

    Replace network_architectures.py

--- src/modules/network_architectures.py
+++ src/modules/network_architectures.py
@@ -1,3 +1,5 @@
+# https://github.com/pytorch/vision/blob/master/torchvision/models/alexnet.py
+
 # for neural network
 import torch
 import torch.nn as nn
@@ -7,20 +9,20 @@ class AlexNet(nn.Module):
 
   def __init__(self, num_classes:int=2, inp_channels:int=3) -> None:
     super(AlexNet, self).__init__()
-    self.features = nn.Sequential(    # inp_len = 50             np.floor((Lin + 2*padding - kernel_size)/stride + 1)
-      nn.Conv1d(inp_channels, 64, kernel_size=11, stride=4, padding=2),   # 50 -> 11 
+    self.features = nn.Sequential(    # inp_len = 500             np.floor((Lin + 2*padding - kernel_size)/stride + 1)
+      nn.Conv1d(inp_channels, 64, kernel_size=11, stride=4, padding=2),   # 500 -> 124
       nn.ReLU(inplace=True),
-      nn.MaxPool1d(kernel_size=3, stride=2),    # 11 -> 5
+      nn.MaxPool1d(kernel_size=3, stride=2),    # 124 -> 61
       nn.Conv1d(64, 192, kernel_size=5, padding=2), # same
       nn.ReLU(inplace=True),
-      nn.MaxPool1d(kernel_size=3, stride=2),  # 5 -> 2
+      nn.MaxPool1d(kernel_size=3, stride=2),  # 61 -> 30
       nn.Conv1d(192, 384, kernel_size=3, padding=1),  # same
       nn.ReLU(inplace=True),
       nn.Conv1d(384, 256, kernel_size=3, padding=1),  # same
       nn.ReLU(inplace=True),
       nn.Conv1d(256, 256, kernel_size=3, padding=1),  # same
       nn.ReLU(inplace=False),   # inplace operation would prevent backward hook
-      nn.MaxPool1d(kernel_size=2, stride=2),    # 2 -> 1
+      nn.MaxPool1d(kernel_size=3, stride=2),    # 30 -> 14
     )
     self.avgpool = nn.AdaptiveAvgPool1d(6)
     self.classifier = nn.Sequential(
