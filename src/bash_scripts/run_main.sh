1e92e46a75a0b905ad944313ea9a50d7c479c7d2
Jwalin Bhatt <bhatt@koeln.kl.dfki.de>
2021-06-29 23:44:56 +0200

    update

--- src/bash_scripts/run_main.sh
+++ src/bash_scripts/run_main.sh
@@ -6,7 +6,6 @@ export PATH=/netscratch/bhatt/Environment/miniconda3/bin/:$PATH
 #working dir
 cd /netscratch/bhatt/Repositories/time-series-attribution/src/scripts/
 
-python main.py  --dataset "UWaveGestureLibraryAll" \
-                        --load True \
-                        --n_samples 1000  \
-                        --gridEvalParams '{"percs":[99,98,96,92], "rand_layers":[0,-1,-2,-3]'}
+python main.py  --gridEvalParams '{"percs":[99,98,96,92], "rand_layers":[0,-1,-2,-3]'} \
+                --load True \
+                --n_samples 1000
